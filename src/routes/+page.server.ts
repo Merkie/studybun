fcfe9bb2b817430eda6bb56fe3292ecc02827285
Merkie <merkie@post.com>
2022-09-01 21:20:38 -0500

    add homepage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,18 @@
 import type { ServerLoad } from '@sveltejs/kit';
+import { client } from '$lib/prisma';
 
 export const load: ServerLoad = async ({ parent }) => {
 	const { lucia } = await parent();
+
 	if (lucia) {
-		return { user: lucia.user };
+		const sets = await client.flashcardSet.findMany({
+			include: {
+				flashcards: true
+			}
+		});
+
+		return { user: lucia.user, sets: sets };
 	}
 
-	return { url: process.env.DISCORD_LOGIN_URL };
+	return { url: process.env.DISCORD_LOGIN_URL, sets: sets };
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,67 @@
 	export let data: { user: IUser; url: string };
 </script>
 
-<svelte:head>
-	<title>Home | Study Bun</title>
-</svelte:head>
-
 <Header discordLoginUrl={data.url} user={data.user} />
+<!-- 
+{#each data.sets as item}
+	<h3>{item.name}</h3>
+	{#each item.flashcards as card}
+		<b>Card: {item.flashcards.indexOf(card)}</b>
+		<p>Term: {card.term}</p>
+		<p>Definition: {card.description}</p>
+	{/each}
+{/each} -->
+
+<main>
+	<h1>All study sets</h1>
+	<span style="display: flex; flex-wrap: wrap; gap: 30px;">
+		{#each data.sets as item}
+			<div on:click={() => window.location.assign('/set/' + item.id)}>
+				<p>
+					{item.name}<span class="subtext">{item.flashcards.length} terms</span>
+				</p>
+				<p style="margin-top: 10px; font-weight: normal;">Created by {item.userId}</p>
+			</div>
+		{/each}
+	</span>
+</main>
+
+<style>
+	main {
+		width: min(75%, 1200px);
+		margin: 0 auto;
+		background-color: var(--surface-background);
+		padding: 30px;
+		margin-top: 20px;
+		border-radius: 10px;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+	}
+
+	div {
+		padding: 20px;
+		background-color: var(--med-pink);
+		border: 1px solid var(--border);
+		border-radius: 10px;
+		font-size: 18px;
+		font-weight: bold;
+		margin: 0;
+		width: fit-content;
+		cursor: pointer;
+		transition-duration: 0.1s;
+	}
+	div:hover {
+		transform: translateY(-3px);
+	}
+
+	p {
+		margin: 0;
+	}
+
+	.subtext {
+		font-size: 16px;
+		font-weight: normal;
+		margin-left: 10px;
+	}
+</style>
